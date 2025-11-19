dfbcc8a71119b179dc7de6081019fcedf1c943aa
TET ELITE <5hibalord69@gmail.com>
2025-11-19 10:23:01 +0700

    exercise1

--- lib/EX-1/ex_1.dart
+++ lib/EX-1/ex_1.dart
@@ -6,15 +6,61 @@ void main() => runApp(MaterialApp(
           title: const Text("Custom buttons"),
         ),
         body: Center(
-          child: SizedBox(
-                width: 400,
-                height: 100,
-                child: ElevatedButton(
-                    onPressed: () => {},
-                    child: const Center(
-                      child: Text("Not Selected"),
-                    )),
-              ),
+          child: Column(
+            children: [
+              SelectButton(),
+              SizedBox(height: 20),
+              SelectButton(),
+              SizedBox(height: 20),
+              SelectButton(),
+              SizedBox(height: 20),
+              SelectButton(),
+            ],
+          ),
         ),
       ),
     ));
+
+class SelectButton extends StatefulWidget {
+  const SelectButton({
+    super.key,
+  });
+
+  @override
+  State<SelectButton> createState() => _SelectButtonState();
+
+}
+
+class _SelectButtonState extends State<SelectButton> {
+
+  bool isSelected = false;
+
+  Color get buttonColor => isSelected ? Colors.blue.shade500 : Colors.blue.shade50;
+  Color get textColor => isSelected ? Colors.white : Colors.black;
+  String get buttonText => isSelected ? "Selected" : "Not Selected";
+
+  void buttonSelection() {
+    setState(() {
+      isSelected = !isSelected; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      height: 100,
+      child: ElevatedButton(
+        onPressed: buttonSelection,  
+        style: ElevatedButton.styleFrom( 
+          backgroundColor: buttonColor,
+          foregroundColor: textColor,
+        ),
+        child: Center(
+          child: Text(buttonText),  
+        ),
+      ),
+    );
+  }
+}
+
